59ab0b9b26a63759323a360a1d98a29440aa7566
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-07-07 13:57:39 +0300

    fix

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -31,7 +31,7 @@ export default {
     data: () => ({
       Password: '',
       rules: [
-        value => {
+        (value:any) => {
           if (value.length >= 8) return true
 
           return 'Validation Message'
